3816afa615e1c84509074eb99acf249659a4f424
lonewolfyx <877085893@qq.com>
2026-03-07 20:46:53 +0800

    feat(meta): enhance eslint rules metadata with detailed configuration

--- src/meta/strategy/eslint.ts
+++ src/meta/strategy/eslint.ts
@@ -1,9 +1,29 @@
+import type { Rule } from 'eslint'
 import type { IOxlintRules, RulesMetaStrategy } from '@/types.ts'
 
 export function EslintRulesMetaConfig(): RulesMetaStrategy {
     return {
-        getRuleMeta(rule: IOxlintRules) {
-            return rule.scope
+        async getRuleMeta(rule: IOxlintRules) {
+            const eslintRules = await import('eslint/use-at-your-own-risk')
+                .then(r => r.default.builtinRules)
+
+            const rulesMeta = eslintRules.get(rule.value) as Rule.RuleModule
+
+            return {
+                name: rule.value,
+                meta: {
+                    source: rule.scope,
+                    category: rule.category,
+                    default: rule.default,
+                    docs: {
+                        description: rulesMeta?.meta?.docs?.description || '',
+                        url: rule.docs_url,
+                        category: rule.category,
+                    },
+                    fixable: rule.fix,
+                    schema: rulesMeta?.meta?.schema || [],
+                },
+            }
         },
     }
 }
