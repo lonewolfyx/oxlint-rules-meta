42caa466e1ee4c12e280e350b41ed179e0d5cf0e
lonewolfyx <877085893@qq.com>
2026-03-07 15:31:18 +0800

    chore: initial project

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+import type { Linter } from 'eslint'
+import antfu from '@antfu/eslint-config'
+
+const config = antfu({
+    stylistic: {
+        indent: 4,
+        quotes: 'single',
+    },
+    rules: {
+        'no-console': 'off',
+        'node/prefer-global/process': 'off',
+        'ts/explicit-function-return-type': 'off',
+        'regexp/no-unused-capturing-group': 'off',
+    },
+    yaml: {
+        overrides: {
+            'yaml/indent': ['error', 2],
+        },
+    },
+}) as Linter.Config
+
+export default config
