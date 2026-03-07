9a051a7e7b2c4aa69689d7c8b952522037bbd5e0
lonewolfyx <877085893@qq.com>
2026-03-07 16:40:59 +0800

    feat(core): initialize oxlint rules configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-export function test() {
-    console.log('hello')
-    console.log(new Date())
+import { name, version } from '../package.json' with { type: 'json' }
+
+const oxlintRules = {
+    name,
+    version,
+    meta: {
+        name,
+        version,
+        url: 'https://oxc.rs/',
+        rules: 'https://oxc.rs/docs/guide/usage/linter/rules.html',
+    },
+    rules: {},
+}
+
+export {
+    oxlintRules,
 }
